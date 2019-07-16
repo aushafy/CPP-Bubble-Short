99650dfdfec262db0f4c063535e30685445ff7b3
Aushafy <42592443+aushafy@users.noreply.github.com>
2019-07-16 21:58:04 +0700

    Add files via upload

---
+++ bubble_sort.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+using namespace std;
+
+void swap(int *xp, int *yp) // membuat fungsi swap
+{ 
+    int temp = *xp;
+    *xp = *yp; 
+    *yp = temp; 
+} 
+  
+// A function to implement bubble sort 
+void bubbleSort(int arr[], int n) 
+{ 
+   int i, j; 
+   for (i = 0; i < n-1; i++)       
+  
+       // Last i elements are already in place    
+       for (j = 0; j < n-i-1; j++)  
+           if (arr[j] > arr[j+1]) // ganti > menjadi '<' untuk mengurutkan data secara descending.
+              swap(&arr[j], &arr[j+1]); 
+} 
+  
+/* Function to print an array */
+void printArray(int arr[], int size) 
+{ 
+    int i; 
+    for (i=0; i < size; i++) 
+        printf("%d ", arr[i]); 
+    printf("\n"); 
+} 
+  
+// Driver program to test above functions 
+int main() 
+{ 
+    int arr[] = {5, 2, 21, 4, 9, 16, 13, 3}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    bubbleSort(arr, n); 
+    printf("Sorted array: \n"); 
+    printArray(arr, n); 
+    return 0; 
+}
